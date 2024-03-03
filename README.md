7a63419ad0b5dee5e70ab36171a4763e4dbc1fc5
jonathan-oberlander <jonathan.oberlander@gmail.com>
2024-03-03 19:08:44 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + TypeScript + Vite + Bun + Biome
+
+<https://biomejs.dev/guides/getting-started/>
+<https://bun.sh/guides/ecosystem/vite>
+
+```shell
+bun dev
+
+bun build
+
+
+```
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>vite-bun-biome-react</h1>
+      <h1>vite-bun-biome-react</h1>
+      <h1>vite-bun-biome-react</h1>
+      <h1>vite-bun-biome-react</h1>
+      <h1>vite-bun-biome-react</h1>
+    </>
+  )
+}
+
+export default App
