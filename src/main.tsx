f1cda0f4ee86805578e6df7e70eece70cdaa488f
jonathan-oberlander <jonathan.oberlander@gmail.com>
2024-04-14 14:57:04 +0100

    fix import error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import { App } from './App.tsx'
-import { CompanyView } from './components/CompanyView.tsx'
+import { CompanyView } from './components/companyView.tsx'
 import { ErrorBoundary } from './components/errorBoundary.tsx'
+
 import './index.css'
 
 const router = createBrowserRouter([
